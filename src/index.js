70743fea38eda530bef6a99a6b29c7b260f5bc2c
ksd-learn <113256544+ksd-learn@users.noreply.github.com>
2023-06-17 13:38:42 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
+import './index.css';
+import { BrowserRouter } from "react-router-dom";
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename='/react-mentor'>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
